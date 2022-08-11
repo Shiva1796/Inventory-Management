c247ba90c1cd872ad757a86b7a44abd94aedbe54
Shiva1796 <shiva96chawla@gmail.com>
2022-08-10 21:46:25 -0400

    Added a firebase database

--- src/components/List/DisplayList.js
+++ src/components/List/DisplayList.js
@@ -1,7 +1,32 @@
+import { useState, useEffect } from "react";
+
 import Card from "../UI/Card";
 import classes from "./DisplayList.modules.css";
 
 const DisplayList = (props) => {
+  const [parts, setLoadedParts] = useState([]);
+  useEffect(() => {
+    const FetchParts = async () => {
+      const response = await fetch(
+        "https://inventory-management-ad87c-default-rtdb.firebaseio.com/parts/"
+      );
+      const responseData = await response.json();
+
+      const loadedParts = [];
+      for (const id in responseData) {
+        loadedParts.push({
+          id: id,
+          partNumber: responseData[id].partNumber,
+          price: responseData[id].price,
+          weight: responseData[id].weight,
+        });
+        setLoadedParts(loadedParts);
+        console.log(responseData);
+      }
+      FetchParts();
+    };
+  }, []);
+
   return (
     <div className={"list"}>
       {props.parts.map((part) => (
